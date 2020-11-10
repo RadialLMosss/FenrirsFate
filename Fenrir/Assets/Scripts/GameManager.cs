53963017c875364db2fd9832e046b958fb8088d1
Rafael FB <rafael.ferreira778@gmail.com>
2020-11-10 14:28:04 -0300

    LevelGenerator

---
+++ Fenrir/Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // progression markers (room index where things get different && HOW it get different)
+    // maybe make it a list of classes and every time the player get to go to a new room we see if there is a marker there
+    // difficulty boost, biome change, boss, dialogue box, shop...
+}
---
+++ Fenrir/Assets/Scripts/LevelSection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSection : MonoBehaviour
+{
+    public void GenerateSection()
+    {
+        // Generate Walls
+        /*
+         * 
+         * free sensors:
+         * west & north gererate big walls (depends on the biome, if surrounded by water it doesn't need big walls)
+         * east & south generate small walls
+         * 
+        */
+        // && foreGround Parallax
+
+        // section design && poppulation + exits & enterings combining (use sensors to do that)
+    }
+}
